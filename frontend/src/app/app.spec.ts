5e75f5b27e996708ee7aeeb443554b5b5749550e
Md Tanjir Mahabub <38369649+tanjir-mahabub@users.noreply.github.com>
2026-09-14 01:47:50 +0600

    feat: update frontend/src/app/app.spec.ts

--- frontend/src/app/app.spec.ts
+++ frontend/src/app/app.spec.ts
@@ -1,3 +1,5 @@
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
-describe('App',()=>{beforeEach(async()=>TestBed.configureTestingModule({imports:[App]}).compileComponents());it('creates OpsFlow',()=>{const f=TestBed.createComponent(App);expect(f.componentInstance).toBeTruthy();expect((f.nativeElement as HTMLElement).textContent).toContain('OpsFlow')})});
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+describe('App',()=>{beforeEach(async()=>TestBed.configureTestingModule({imports:[App],providers:[provideHttpClient(),provideHttpClientTesting()]}).compileComponents());it('creates OpsFlow',()=>{const f=TestBed.createComponent(App);expect(f.componentInstance).toBeTruthy();expect((f.nativeElement as HTMLElement).textContent).toContain('OpsFlow')})});
